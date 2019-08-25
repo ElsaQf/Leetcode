44e4e0b4d99f52c278b02e755523eef6feb293e5
QuFang <melody.qf@163.com>
2019-08-25 20:26:04 +0800

    增加优化算法

--- 0011ContainerWithMostWater.cpp
+++ 0011ContainerWithMostWater.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    # 暴力
     int maxArea(vector<int>& height) {
         int area = 0;
         int n = height.size();
@@ -9,6 +10,26 @@ public:
                 if(a > area){
                     area = a;
                 }
+            }
+        }
+        return area;
+    }
+    # 前后向中间逼近（优化）
+    int maxArea_2(vector<int>& height) {
+        int l = 0, r = height.size() - 1;
+        int area = 0;
+
+        while(l < r){
+            int a = 0;
+            if(height[l] < height[r]){
+                a = height[l] * (r - l);
+                l++;
+            }else{
+                a = height[r] * (r - l);
+                r--;
+            }
+            if(a > area){
+                area = a;
             }
         }
         return area;
